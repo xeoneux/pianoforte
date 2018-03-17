4358d7349606d077f374874957d23475c6d4236a
Aayush Kapoor <xeoneux@icloud.com>
2018-03-17 19:45:37 +0530

    Add basic piano keys

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ import Piano from './components/Piano/Piano';
 class App extends Component {
   render() {
     return (
-      <div class="App">
-        <div class="App-header">
+      <div className="App">
+        <div className="App-header">
           <h1>Header</h1>
         </div>
-        <div class="App-content">
+        <div className="App-content">
           <h1>Content</h1>
         </div>
-        <div class="App-footer">
-          <h1>Footer</h1>
+        <div className="App-footer">
+          <Piano />
         </div>
       </div>
     );
--- src/components/Piano/Piano.js
+++ src/components/Piano/Piano.js
@@ -1,34 +1,58 @@
+import glamorous from 'glamorous';
 import React, { Component } from 'react';
-
-import './Piano.css';
 
 const whites = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
 const blacks = ['C♯/D♭', 'D♯/E♭', 'F♯/G♭', 'G♯/A♭', 'A♯/B♭'];
 const octave = [
-  whites[0],
-  blacks[0],
-  whites[1],
-  blacks[1],
-  whites[2],
-  whites[3],
-  blacks[2],
-  whites[4],
-  blacks[3],
-  whites[5],
-  blacks[4],
-  whites[6]
+  { type: 'white', name: whites[0], index: 0 },
+  { type: 'black', name: blacks[0], index: 0 },
+  { type: 'white', name: whites[1], index: 1 },
+  { type: 'black', name: blacks[1], index: 1 },
+  { type: 'white', name: whites[2], index: 2 },
+  { type: 'white', name: whites[3], index: 3 },
+  { type: 'black', name: blacks[2], index: 2 },
+  { type: 'white', name: whites[4], index: 4 },
+  { type: 'black', name: blacks[3], index: 3 },
+  { type: 'white', name: whites[5], index: 5 },
+  { type: 'black', name: blacks[4], index: 4 },
+  { type: 'white', name: whites[6], index: 6 }
 ];
 
 class Piano extends Component {
   render() {
     return (
-      <div class="keyboard">
-        <Key />
+      <div className="Piano">
+        {octave.map(data => <Key key={data.name} {...data} />)}
       </div>
     );
   }
 }
 
-const Key = props => <div class="Piano-key" />;
+const keyHeight = 30;
+const keyWidth = keyHeight / 4;
+const crossHeightRatio = 0.75;
+const crossWidthRatio = 0.66;
+
+const Key = glamorous.div(
+  {
+    cursor: 'pointer',
+    position: 'absolute',
+    border: '1px solid gray',
+    borderRadius: '0 0 5px 5px',
+    boxShadow: '2px 2px 2px rgba(0, 0, 0, 0.3)'
+  },
+  ({ type = 'white', index }) => ({
+    left:
+      type === 'black'
+        ? index * keyWidth + (keyWidth - keyWidth * crossWidthRatio / 2)
+        : index * keyWidth,
+    zIndex: type === 'black' ? 100 : 90,
+    background: type === 'black' ? 'black' : 'white',
+    height:
+      type === 'black' ? `${keyHeight * crossHeightRatio}vh` : `${keyHeight}vh`,
+    width:
+      type === 'black' ? `${keyWidth * crossWidthRatio}vh` : `${keyWidth}vh`
+  })
+);
 
 export default Piano;
