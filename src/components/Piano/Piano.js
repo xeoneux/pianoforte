a3a0afdaac921eb4c667e615f50b2996415f501b
Aayush Kapoor <xeoneux@icloud.com>
2018-03-18 00:21:36 +0530

    Separate piano config file

--- src/components/Piano/Piano.js
+++ src/components/Piano/Piano.js
@@ -1,73 +1,27 @@
 import Glamorous, { Div } from 'glamorous';
 import React, { Component } from 'react';
 
-const whites = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
-const blacks = ['C♯|D♭', 'D♯|E♭', 'F♯|G♭', 'G♯|A♭', 'A♯|B♭'];
-const octave = [
-  { type: 'white', name: whites[0], pos: 0 },
-  { type: 'black', name: blacks[0], pos: 0 },
-  { type: 'white', name: whites[1], pos: 1 },
-  { type: 'black', name: blacks[1], pos: 1 },
-  { type: 'white', name: whites[2], pos: 2 },
-  { type: 'white', name: whites[3], pos: 3 },
-  { type: 'black', name: blacks[2], pos: 2 },
-  { type: 'white', name: whites[4], pos: 4 },
-  { type: 'black', name: blacks[3], pos: 3 },
-  { type: 'white', name: whites[5], pos: 5 },
-  { type: 'black', name: blacks[4], pos: 4 },
-  { type: 'white', name: whites[6], pos: 6 }
-];
-
-// TODO: Refactor in helper...
-const generatePos = idx => {
-  const pattern = ['w', 'b', 'w', 'b', 'w', 'w', 'b', 'w', 'b', 'w', 'b', 'w'];
-  let quotient = Math.floor(idx / pattern.length);
-  let remainder = idx % pattern.length;
-
-  if (remainder === 0) {
-    quotient = quotient - 1;
-    remainder = pattern.length;
-  }
-  const key = pattern[remainder - 1];
-
-  let pre;
-  if (key === 'w') pre = quotient * whites.length;
-  if (key === 'b') pre = quotient * blacks.length;
-
-  let count = 0;
-  pattern.forEach((note, index) => {
-    if (note === key && index < remainder) count++;
-  });
-  return pre + count;
-};
-
-const octaves = [...Array(7)].map((_, num) =>
-  octave.map((key, idx) =>
-    Object.assign({}, key, {
-      name: key.name + num,
-      pos: generatePos(idx + 1 + num * 7) - 1
-    })
-  )
-);
+import {
+  keyWidth,
+  keyHeight,
+  crossWidthRatio,
+  crossHeightRatio,
+  generateKeyboard
+} from '../../config/Piano';
 
 class Piano extends Component {
   render() {
     return (
       <Div>
-        {[].concat(...octaves).map((val, idx) => (
-          <Key {...val} idx={idx} key={val.name}>
-            <Name>{val.name}</Name>
+        {generateKeyboard().map((value, index) => (
+          <Key {...value} index={index} key={value.key}>
+            <Name>{value.name}</Name>
           </Key>
         ))}
       </Div>
     );
   }
 }
-
-const keyHeight = 30;
-const keyWidth = keyHeight / 4;
-const crossHeightRatio = 0.75;
-const crossWidthRatio = 0.66;
 
 const Key = Glamorous.div(
   {
@@ -77,11 +31,11 @@ const Key = Glamorous.div(
     borderRadius: '0 0 5px 5px',
     boxShadow: '2px 2px 2px rgba(0, 0, 0, 0.3)'
   },
-  ({ type, idx, pos }) => ({
+  ({ type, index, position }) => ({
     left:
       type === 'white'
-        ? `${pos * keyWidth}vh`
-        : `${(idx - pos - 1) * keyWidth +
+        ? `${position * keyWidth}vh`
+        : `${(index - position - 1) * keyWidth +
             (keyWidth - keyWidth * crossWidthRatio / 2)}vh`,
     zIndex: type === 'white' ? 90 : 100,
     background: type === 'white' ? 'white' : 'black',
---
+++ src/config/Piano.js
@@ -0,0 +1,42 @@
+/*
+
+MIDI Standard for Note Numbers
+
+Oct	C	C#	D	D#	E	F	F#	G	G#	A	A#	B
+-1	0	1	2	3	4	5	6	7	8	9	10	11
+0	12	13	14	15	16	17	18	19	20	21	22	23
+1	24	25	26	27	28	29	30	31	32	33	34	35
+2	36	37	38	39	40	41	42	43	44	45	46	47
+3	48	49	50	51	52	53	54	55	56	57	58	59
+4	60*	61	62	63	64	65	66	67	68	69!	70	71
+5	72	73	74	75	76	77	78	79	80	81	82	83
+6	84	85	86	87	88	89	90	91	92	93	94	95
+7	96	97	98	99	100	101	102	103	104	105	106	107
+8	108	109	110	111	112	113	114	115	116	117	118	119
+9	120	121	122	123	124	125	126	127
+*/
+
+export const keyHeight = 30;
+export const crossWidthRatio = 2 / 3;
+export const crossHeightRatio = 2 / 3;
+export const keyWidth = keyHeight / 4;
+
+export const whites = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
+export const blacks = ['C♯|D♭', 'D♯|E♭', 'F♯|G♭', 'G♯|A♭', 'A♯|B♭'];
+
+export const generateKeyboard = ({ startKey = 0, endKey = 127 } = {}) => {
+  return [
+    { key: 0, name: 'C', type: 'white', position: 0 },
+    { key: 1, name: 'C#|Db', type: 'black', position: 0 },
+    { key: 2, name: 'D', type: 'white', position: 1 },
+    { key: 3, name: 'D#|Eb', type: 'black', position: 1 },
+    { key: 4, name: 'E', type: 'white', position: 2 },
+    { key: 5, name: 'F', type: 'white', position: 3 },
+    { key: 6, name: 'F♯|G♭', type: 'black', position: 2 },
+    { key: 7, name: 'G', type: 'white', position: 4 },
+    { key: 8, name: 'G♯|A♭', type: 'black', position: 3 },
+    { key: 9, name: 'A', type: 'white', position: 5 },
+    { key: 10, name: 'A♯|B♭', type: 'black', position: 4 },
+    { key: 11, name: 'B', type: 'white', position: 6 }
+  ];
+};
