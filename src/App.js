619992436ff9a3330fa000cd94162e0142473f5b
Aayush Kapoor <xeoneux@icloud.com>
2018-03-17 14:05:12 +0530

    Set basic layout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+
+import Piano from './components/Piano/Piano';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div class="App">
+        <div class="App-header">
+          <h1>Header</h1>
+        </div>
+        <div class="App-content">
+          <h1>Content</h1>
+        </div>
+        <div class="App-footer">
+          <h1>Footer</h1>
+        </div>
       </div>
     );
   }
