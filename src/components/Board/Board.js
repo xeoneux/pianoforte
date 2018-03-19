97ad8a655ec8e497f4e0a770032d051c4ba7d5e6
Aayush Kapoor <xeoneux@icloud.com>
2018-03-19 14:03:56 +0530

    Fix vertical line duplication

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -8,8 +8,8 @@ class Board extends Component {
   render() {
     return (
       <Background>
-        {[...Array(6)].map((_, i) => <FatLine count={i + 1} />)}
-        {[...Array(7)].map((_, i) => <ThinLine count={i} />)}
+        {[...Array(6)].map((_, i) => <Line key={i} count={i + 1} />)}
+        {[...Array(7)].map((_, i) => <Line key={i} type="thin" count={i} />)}
       </Background>
     );
   }
@@ -21,22 +21,15 @@ const Background = Glamorous.div({
   backgroundColor: '#303030'
 });
 
-const FatLine = props => (
-  <Div
-    left={`calc(${props.count * 7 * keyWidth}vh - 1px)`}
-    height={`${contentHeight}vh`}
-    position="absolute"
-    borderLeft="2px solid #5A5A5A"
-  />
-);
-
-const ThinLine = props => (
-  <Div
-    left={`${props.count * 7 * keyWidth + 3 * keyWidth}vh`}
-    height={`${contentHeight}vh`}
-    position="absolute"
-    borderLeft="1px solid #5A5A5A"
-  />
+const Line = Glamorous.div(
+  { position: 'absolute', height: `${contentHeight}vh` },
+  ({ type, count }) => ({
+    left:
+      type === 'thin'
+        ? `${count * 7 * keyWidth + 3 * keyWidth}vh`
+        : `calc(${count * 7 * keyWidth}vh - 1px)`,
+    borderLeft: type === 'thin' ? '1px solid #5A5A5A' : '2px solid #5A5A5A'
+  })
 );
 
 export default Board;
