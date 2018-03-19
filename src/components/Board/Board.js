7c35dc7e83b7d0d6e9cfacf5fbd1858e60e0c3eb
Aayush Kapoor <xeoneux@icloud.com>
2018-03-19 16:37:15 +0530

    Add marker bar value

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -5,15 +5,19 @@ import { contentHeight } from '../../config/app';
 import { keyWidth } from '../../config/piano';
 
 class Board extends Component {
-  state = { markerPercentage: 25 };
+  state = { currentBar: 3, markerPercentage: 25 };
 
   render() {
     return (
       <Background>
         {[...Array(7)].map((_, i) => <Line key={i} count={i + 1} />)}
         {[...Array(8)].map((_, i) => <Line key={i} type="thin" count={i} />)}
-        <Marker type="odd" percentage={this.state.markerPercentage} />
-        <Marker type="even" percentage={this.state.markerPercentage} />
+        <Marker type="odd" percentage={this.state.markerPercentage}>
+          <Bar>{this.state.currentBar}</Bar>
+        </Marker>
+        <Marker type="even" percentage={this.state.markerPercentage}>
+          <Bar>{this.state.currentBar + 1}</Bar>
+        </Marker>
       </Background>
     );
   }
@@ -42,5 +46,19 @@ const Marker = Glamorous.div(
     top: type === 'even' ? `${percentage}%` : `${percentage + 66}%`
   })
 );
+
+const Bar = Glamorous.div({
+  left: 0,
+  bottom: 0,
+  opacity: 0.8,
+  color: 'gray',
+  width: '16px',
+  height: '16px',
+  fontSize: '10px',
+  lineHeight: '16px',
+  fontWeight: 'bold',
+  textAlign: 'center',
+  position: 'absolute'
+});
 
 export default Board;
