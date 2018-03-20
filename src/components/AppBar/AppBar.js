f33bc0c742f3e9ba3df61442930b42390ee50f7e
Aayush Kapoor <xeoneux@icloud.com>
2018-03-20 16:42:46 +0530

    Add progress bar blocks

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -4,16 +4,12 @@ import { Div } from 'glamorous';
 import Controls from './Controls';
 import Progress from './Progress';
 
-import { headerHeight } from '../../config/app';
-
 export default class AppBar extends Component {
   render() {
     return (
       <Div width="100%" height="100%" display="flex" flexDirection="column">
         <Controls />
-        <Div display="flex" height={`${headerHeight / 2}vh`}>
-          <Progress />
-        </Div>
+        <Progress />
       </Div>
     );
   }
--- src/components/AppBar/Progress.js
+++ src/components/AppBar/Progress.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
-import { Div } from 'glamorous';
+import Glamorous, { Div } from 'glamorous';
 
-export default class Controls extends Component {
+const measures = 40;
+
+export default class Progress extends Component {
   render() {
-    return <Div backgroundColor="brown" />;
+    return (
+      <Div height="50%" display="flex">
+        {[...Array(measures)].map((_, i) => <Block key={i} />)}
+      </Div>
+    );
   }
 }
+
+const Block = Glamorous.div({
+  zIndex: 50,
+  height: '100%',
+  border: '1px solid',
+  borderRadius: '1px',
+  backgroundColor: '#303030',
+  width: `${100 / measures}vw`,
+  boxShadow: 'inset 0 1px rgba(255, 255, 255, 0.4)',
+  backgroundImage: `linear-gradient(
+      to bottom,
+      rgba(255, 255, 255, 0.3),
+      rgba(255, 255, 255, 0) 50%,
+      rgba(0, 0, 0, 0.12) 51%,
+      rgba(0, 0, 0, 0.04)
+    )`
+});
