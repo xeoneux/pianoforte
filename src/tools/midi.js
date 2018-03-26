367e28b9e0d08c4cf87dfb9b689ef47191e6bfdc
Aayush Kapoor <xeoneux@icloud.com>
2018-03-26 15:00:28 +0530

    Generate note map for midi

---
+++ src/tools/midi.js
@@ -0,0 +1,37 @@
+export const midiNotesMap = player => {
+  const ticks = player.totalTicks;
+  const division = player.division;
+  const measure = division * 4;
+
+  const data = [];
+  const events = player.getEvents();
+  const state = { start: 0, running: 1, end: 2 };
+
+  events.forEach((trackData, trackIndex) => {
+    data.push([]);
+
+    trackData.forEach(event => {
+      const currentMeasure = Math.floor(event.tick / (measure + 1));
+      const selectedMeasure = data[trackIndex][currentMeasure];
+      if (!selectedMeasure) selectedMeasure = {};
+
+      if (event.name === 'Note on' && event.velocity !== 0) {
+        if (!selectedMeasure[event.noteNumber])
+          selectedMeasure[event.noteNumber] = [];
+        selectedMeasure[event.noteNumber].push([
+          state.start,
+          event.tick,
+          event.velocity
+        ]);
+      }
+
+      if (event.name === 'Note off' || event.velocity === 0) {
+        if (!selectedMeasure[event.noteNumber])
+          selectedMeasure[event.noteNumber] = [];
+        selectedMeasure[event.noteNumber].push([state.end, event.tick]);
+      }
+    });
+  });
+
+  return data;
+};
